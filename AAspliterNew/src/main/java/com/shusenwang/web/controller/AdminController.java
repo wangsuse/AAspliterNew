d3f93889d6ac0b8eeecb879356f9a14f030802c6
wangsuse <wangsuse2011@gmail.com>
2014-07-12 18:34:21 -0400

    first working version

--- AAspliterNew/src/main/java/com/shusenwang/web/controller/AdminController.java
+++ AAspliterNew/src/main/java/com/shusenwang/web/controller/AdminController.java
@@ -5,14 +5,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
+@RequestMapping("admin")
 public class AdminController {
 
-    @RequestMapping(value="/helloWorld", method = RequestMethod.GET)
-    public String helloWorld(Model model) {
-        model.addAttribute("message", "Hello World!");
-        return "helloWorld";
+    @RequestMapping(value="/manager", method = RequestMethod.GET)
+    public ModelAndView helloWorld(Model model) {
+        //model.addAttribute("message", "Hello World!");
+        return new ModelAndView("/admin");
     }
 }
